157c0d2780ca11eaff0e33d2d70fae7897fe38c5
DaniGadget18 <dani_181298@hotmail.com>
2020-04-07 20:02:36 -0500

    modificaciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,16 @@
+// modulos de la app
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-
-
-
 import { CollapseModule } from 'ngx-bootstrap/collapse';
 import { ToastrModule } from 'ngx-toastr';
-
 import { AppRoutingModule } from './app-routing.module';
 
-import { GamesComponent } from './pages/games/games/games.component';
-import { EditGameComponent } from './pages/games/editGame/editGame.component';
 
+// componentes del cascaron
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
@@ -25,17 +21,15 @@ import { FormsComponent } from './pages/forms/forms.component';
 import { TypographyComponent } from './pages/typography/typography.component';
 import { NotificationsComponent } from './pages/notifications/notifications.component';
 
-
+// componenente login
 import { LoginComponent } from './components/login/login.component';
+// Componentes de la aplicacion
+import { GamesComponent } from './pages/games/games/games.component';
+import { EditGameComponent } from './pages/games/editGame/editGame.component';
 import { RegisterComponent } from './components/register/register.component';
 import {LoginGuard} from './login.guard';
 import { CreateGameComponent } from './pages/games/createGame/createGame.component';
 import { UsersComponent } from './pages/users/users/users.component';
-
-import { ComponentesComponent } from './componentes/componentes.component';
-import { ComponentsComponent } from './components/components.component';
-import { RegisterusersComponent } from './registerusers/registerusers.component';
-
 import { EditUserComponent } from './pages/users/edit-user/edit-user.component';
 
 
@@ -57,11 +51,6 @@ import { EditUserComponent } from './pages/users/edit-user/edit-user.component';
     EditGameComponent,
     CreateGameComponent,
     UsersComponent,
-
-    ComponentesComponent,
-    ComponentsComponent,
-    RegisterusersComponent
-
     EditUserComponent
 
 
